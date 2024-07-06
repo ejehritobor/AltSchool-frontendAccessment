f8aa44986f1f917b97f1b24b1e2b6a051c88e2ab
unknown <kufe4tega@gmail.com>
2024-07-06 11:45:44 +0100

    commit at the end of my project

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { NavLink} from "react-router-dom";
+import logo from "../assets/Dav-pic.jpg";
+
+
+function Header() {
+
+  return (
+    <>
+      <header className="App-header">
+        <div className="container">
+          <div className="logo-wrap">
+            <a href="#">
+              <img src={logo} alt="Site logo" />
+            </a>
+            <div className="appName"><p>Github Repo</p></div>
+          </div>
+          
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">Test 404 page</NavLink>
+              </li> 
+            </ul>
+          </nav>
+        </div>
+
+      </header>
+    </>
+  );
+}
+
+export default Header;
